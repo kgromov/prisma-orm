4f14eed6a7fe5ca16448b71da9a3021f182f78ac
konstantin <konst.gromov@gmail.com>
2026-01-20 01:37:14 +0200

    Start working on importing schema from existing db

--- script.ts
+++ script.ts
@@ -1,32 +1,7 @@
 import { prisma } from './prisma/client'
 
 async function main() {
-    // Create a new user with a post
-    const user = await prisma.user.create({
-        data: {
-            name: 'Alice',
-            email: 'alice@prisma.io',
-            posts: {
-                create: {
-                    title: 'Hello World',
-                    content: 'This is my first post!',
-                    published: true,
-                },
-            },
-        },
-        include: {
-            posts: true,
-        },
-    })
-    console.log('Created user:', user)
-
-    // Fetch all users with their posts
-    const allUsers = await prisma.user.findMany({
-        include: {
-            posts: true,
-        },
-    })
-    console.log('All users:', JSON.stringify(allUsers, null, 2))
+   // TODO: write custom queries
 }
 
 main()
